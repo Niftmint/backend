73c0cecc125db58016afcc92d34b1e7af65d01d7
Rui Maximo <rmaximo@lync-solutions.com>
2021-04-03 22:56:52 -0700

    Add files via upload

--- code/main.py
+++ code/main.py
@@ -1,23 +1,88 @@
-import flask
+import plaid
+from flask import Flask
+from flask import request
+from flask import jsonify
+from flask import make_response
+
+app = Flask(__name__)
+
+# CONFIGURATION SETTINGS
+PLAID_REDIRECT_URI='http://localhost:3000/'
+PLAID_CLIENT_ID='6069040283c0da001182aece'
+PLAID_SECRET='3be943a36853176b520a107a88316b'
+PLAID_ENV='sandbox'
+PLAID_PRODUCTS=['transactions']
+
+access_token = None
+item_id = None
+
+client = plaid.Client(client_id=PLAID_CLIENT_ID,
+                      secret=PLAID_SECRET,
+                      environment=PLAID_ENV)
+
+def format_error(e):
+    return {
+        'error': {
+            'display_message': e.display_message, 
+            'error_code': e.code,
+            'error_type': e.type,
+            'error_message': e.message
+            }
+        }
+
+def create_link_token(request):
+    # UPDATE
+    client_user_id = 'user-id'
+
+    try:
+        # Create a link_token for user
+        response = client.LinkToken.create({
+          'user': {
+            'client_user_id': client_user_id,
+          },
+          'products': PLAID_PRODUCTS,
+          'client_name': 'Niftmint',
+          'country_codes': ['US', 'CA'],
+          'language': 'en',
+          'redirect_uri': PLAID_REDIRECT_URI,
+        })
+        link_token = response['link_token']
+
+        # Send the data to the client
+        return jsonify(response)
+    except plaid.errors.PlaidError as e:
+        message = jsonify(format_error(e))
+        return make_response(message, 400)
 
 
-def buy(request):
-    """HTTP Cloud Function.
-    Args:
-        request (flask.Request): The request object.
-        <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
-    Returns:
-        The response text, or any set of values that can be turned into a
-        Response object using `make_response`
-        <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
-    """
-    request_args = request.args
+# Exchange token flow - exchange a Link public_token for
+# an API access_token
+# https://plaid.com/docs/#exchange-token-flow
+def set_access_token(request):
+    global access_token
+    global item_id
+    public_token = None
+    
+    if request.args and 'public_token' in request.args:
+        # get public token from client
+        public_token = request.args.get('public_token')
 
-    if request_args and "nft" in request_args and "buyer" in request_args:
-        nft = request_args["nft"]
-        buyer = request_args["buyer"]
-    else:
-        nft = "666"
-        buyer = "Rui"
-        
-    return "Buy NFT {} for {}".format(flask.escape(nft), flask.escape(buyer))
+    try:
+        exchange_response = client.Item.public_token.exchange(public_token)
+    except plaid.errors.PlaidError as e:
+        message = jsonify(format_error(e))
+        return make_response(message, 400)
+
+    access_token = exchange_response['access_token']
+    item_id = exchange_response['item_id']
+
+    return jsonify(exchange_response)
+
+def info():
+    global access_token
+    global item_id
+    return jsonify({
+        'item_id': item_id,
+        'access_token': access_token,
+        'products': PLAID_PRODUCTS
+        })
