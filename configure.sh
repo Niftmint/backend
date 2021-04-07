37168d0b71db3653dc71d71bbdbf22e98ceb0911
Rui Maximo <rui@maximo.ws>
2021-04-06 18:34:28 -0700

    Create configure.sh
    Bash script to quickly configure all Google Cloud Functions. Mark this script as executable:
    
    > chmod +x configure.sh
    
    Run script every time any of the function logic is modified:
    
    > ./configure.sh

---
+++ configure.sh
@@ -0,0 +1,31 @@
+clear
+error_exit()
+{
+    echo "$1" 1>&2
+    exit 1
+}
+echo configure:
+echo
+gcloud functions deploy info --runtime python39 --trigger-http --allow-unauthenticated || \
+    error_exit "Error deploying function: info"
+echo
+echo
+gcloud functions deploy create_link_token --runtime python39 --trigger-http --allow-unauthenticated || \
+    error_exit "Error deploying function: create_link_token"
+echo
+echo
+gcloud functions deploy set_access_token --runtime python39 --trigger-http --allow-unauthenticated || \
+    error_exit "Error deploying function: set_access_token"
+echo
+echo
+gcloud functions deploy auth --runtime python39 --trigger-http --allow-unauthenticated || \
+    error_exit "Error deploying function: auth"
+echo
+echo
+gcloud functions deploy identity --runtime python39 --trigger-http --allow-unauthenticated || \
+    error_exit "Error deploying function: identity"
+echo
+echo
+gcloud functions deploy balance --runtime python39 --trigger-http --allow-unauthenticated || \
+    error_exit "Error deploying function: balance"
+echo
