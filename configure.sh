18723fa75d51297e8f3842f5996e100ce3de2343
Rui Maximo <rui@maximo.ws>
2021-04-11 09:07:39 -0700

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -16,18 +16,7 @@ echo
 echo
 gcloud functions deploy set_access_token --runtime python39 --trigger-http --allow-unauthenticated || \
     error_exit "Error deploying function: set_access_token"
-echo
-echo
-gcloud functions deploy auth --runtime python39 --trigger-http --allow-unauthenticated || \
-    error_exit "Error deploying function: auth"
-echo
-echo
-gcloud functions deploy identity --runtime python39 --trigger-http --allow-unauthenticated || \
-    error_exit "Error deploying function: identity"
-echo
-echo
-gcloud functions deploy balance --runtime python39 --trigger-http --allow-unauthenticated || \
-    error_exit "Error deploying function: balance"
+
 echo
 echo done
 echo
